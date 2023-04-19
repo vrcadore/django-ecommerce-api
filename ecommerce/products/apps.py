38dfd0b0c5357315c025ae1837ff720334a833e3
vrcadore <vrcadore@users.noreply.github.com>
2023-04-19 16:50:47 +0000

    deploy: 1854a7709acf7ee2b6ca394f3244af3d890a29b6

--- ecommerce/products/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class ProductConfig(AppConfig):
-    name = "ecommerce.products"
-    verbose_name = _("Products")
